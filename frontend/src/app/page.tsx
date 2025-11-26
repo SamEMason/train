f77955cbda0d04a24804245960f4bb0435e4d9bc
sam <me@sammason.dev>
2025-11-26 14:56:30 -0500

    initialize next.js frontend with tailwind, ts, and shadcn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-900 font-sans text-white dark:bg-black">
